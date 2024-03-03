92573cfeae8cd80d6f7c390375229019f3137102
ashik-maz <ashik.cse.pust@gmail.com>
2024-03-03 17:52:10 +0600

    Changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:bmi/bmi.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -18,20 +17,119 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  String str = '_ _ _ _';
+  final h = TextEditingController();
+  final w = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-          Image.asset(
-            "images/bg1.png",
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            fit: BoxFit.cover,
+          Image.asset("images/bg1.png",
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          fit: BoxFit.cover,
           ),
           bmi(),
         ],
       ),
     );
+  }
+  
+  bmi() {
+    return SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Text(
+                  'BMI CALCULATOR',
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.w700),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Enter Your height',style: TextStyle(fontSize: 12),),
+                      SizedBox(height: 5,),
+                      TextField(
+                        decoration: InputDecoration(
+                          //label: Text('Height in cm?'),
+                          hintText: 'Height in cm?',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                            borderSide: BorderSide(width: 2, color: Colors.pink),
+                          ),
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 2, color: Color(0xFF005506)))
+                        ),
+                        controller: h,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Enter Your Weight',style: TextStyle(fontSize: 12,),),
+                      SizedBox(height: 5,),
+                      TextField(
+                        decoration: InputDecoration(
+                          //label: Text('Weight in kg?'),
+                          hintText: 'Weight in kg?',
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(5.0),
+                            ),
+                            borderSide: BorderSide(width: 2, color: Colors.pink),
+                          ),
+                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(width: 2, color: Color(0xFF005506)))
+                        ),
+                        controller: w,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      str =
+                        '${ (int.parse(w.text) / ((int.parse(h.text) / 100) * (int.parse(h.text) / 100))).toStringAsFixed(3)}';
+                          
+                    });
+                  },
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.pink),
+                  child: Padding(
+                    padding:
+                        EdgeInsets.symmetric(vertical: 5.0, horizontal: 20.0),
+                    child: Text(
+                      'Calculate',
+                      style: TextStyle(fontSize: 30, color: Colors.white),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 250,
+                ),
+                Text(
+                  'YOUR BMI',
+                  style: TextStyle(
+                      fontSize: 40,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500),
+                ),
+                 Text(
+                    str,
+                    style: TextStyle(fontSize: 60,fontWeight: FontWeight.w700,color: Colors.white),
+                  ),
+              ],
+            ),
+          );
   }
 }
